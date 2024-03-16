c9d47412dd550d85de7165bbaec03a55536e3bc4
Dorian Gillioz <gilliozdorian@gmail.com>
2024-03-16 17:44:45 +0100

    1.4 deflater et refacto du code

--- code/app/src/main/java/ch/heigvd/iict/dma/labo1/repositories/MeasuresRepository.kt
+++ code/app/src/main/java/ch/heigvd/iict/dma/labo1/repositories/MeasuresRepository.kt
@@ -1,18 +1,14 @@
 package ch.heigvd.iict.dma.labo1.repositories
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.map
 import ch.heigvd.iict.dma.labo1.models.*
-import ch.heigvd.iict.dma.protobuf.MeasuresAckKt
-import ch.heigvd.iict.dma.protobuf.MeasuresKt
 import ch.heigvd.iict.dma.protobuf.MeasuresOuterClass.MeasuresAck
 import ch.heigvd.iict.dma.protobuf.MeasuresOuterClass.Status
 import ch.heigvd.iict.dma.protobuf.measure
 import ch.heigvd.iict.dma.protobuf.measures
-import com.google.firebase.encoders.proto.Protobuf
-import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import com.google.gson.reflect.TypeToken
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -32,7 +28,12 @@ import org.jdom2.output.XMLOutputter
 import java.io.DataInputStream
 import java.io.DataOutputStream
 import java.io.StringReader
-import java.text.Format
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.zip.Deflater
+import java.util.zip.DeflaterOutputStream
+import java.util.zip.Inflater
+import java.util.zip.InflaterInputStream
 
 class MeasuresRepository(private val scope : CoroutineScope,
                          private val dtd : String = "https://mobile.iict.ch/measures.dtd",
@@ -67,6 +68,10 @@ class MeasuresRepository(private val scope : CoroutineScope,
         _measures.postValue(mutableListOf())
     }
 
+    fun toSimpleString(date: Date) = with(date) {
+        SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").format(this)
+    }
+
     fun sendMeasureToServer(encryption : Encryption, compression : Compression, networkType : NetworkType, serialisation : Serialisation) {
         scope.launch(Dispatchers.Default) {
 
@@ -74,174 +79,168 @@ class MeasuresRepository(private val scope : CoroutineScope,
                 Encryption.DISABLED -> httpUrl
                 Encryption.SSL -> httpsUrl
             }
-            val user_agent = "Dorian"
+
+            val measureList = _measures.value!!
+
+            val gson = GsonBuilder()
+                .setDateFormat("yyyy-MM-dd'T'HH:mm:ss")
+                .create()
+
             val elapsed = measureTimeMillis {
-                Log.e("SendViewModel", "Implement me !!! Send measures to $url") //TODO
-                when (serialisation) {
-                    Serialisation.JSON -> {
-                        val gson = Gson()
+                with(URL(url).openConnection() as HttpURLConnection) {
+                    requestMethod = "POST"
+                    setRequestProperty("Content-Type", when (serialisation) {
+                        Serialisation.JSON -> "application/json; charset=utf-8"
+                        Serialisation.XML -> "application/xml; charset=utf-8"
+                        Serialisation.PROTOBUF -> "application/protobuf"
+                    })
+                    setRequestProperty("User-Agent", "Dorian")
+                    setRequestProperty("X-Network", networkType.toString())
+                    setRequestProperty("X-Content-Encoding", when(compression) {
+                        Compression.DISABLED -> "NONE"
+                        Compression.DEFLATE -> "DEFLATE"
+                    })
 
-                        val measures = _measures.value!!
+                    try {
+                        // Enable output (sending data to server)
+                        doOutput = true
 
-                        with(URL(url).openConnection() as HttpURLConnection) {
-                            requestMethod = "POST"
-                            setRequestProperty("Content-Type", "application/json; charset=utf-8")
-                            setRequestProperty("User-Agent", user_agent)
+                        // Write JSON data to the connection's output stream
+                        var newOutPutStream = outputStream
 
-                            try {
-                                // Enable output (sending data to server)
-                                doOutput = true
+                        if (compression == Compression.DEFLATE) {
+                            newOutPutStream = DeflaterOutputStream(newOutPutStream, Deflater(Deflater.DEFAULT_COMPRESSION, true))
+                        }
 
-                                // Write JSON data to the connection's output stream
-                                OutputStreamWriter(outputStream, "UTF-8").use { writer ->
-                                    writer.write(gson.toJson(measures)) //it
+                        when (serialisation) {
+                            Serialisation.JSON -> {
+                                OutputStreamWriter(newOutPutStream, "UTF-8").use { writer ->
+                                    writer.write(gson.toJson(measureList)) //it
                                     writer.flush()
                                 }
-
-                                // Read the response from the server
-                                BufferedReader(InputStreamReader(inputStream)).use { reader ->
-                                    val response = StringBuilder()
-                                    var line: String?
-                                    while (reader.readLine().also { line = it } != null) {
-                                        response.append(line)
-                                    }
-                                    // Handle response
-                                    val res = gson.fromJson<List<MeasureACK>>(response.toString(), object : TypeToken<List<MeasureACK>>() {}.type)
-
-                                    for (r in res) {
-                                        measures[r.id].status = r.status
-                                    }
-                                    _measures.postValue(measures)
-                                }
-                            } catch (e: IOException) {
-                                e.printStackTrace()
-                            } finally {
-                                disconnect()
                             }
-                        }
+                            Serialisation.XML -> {
+                                val rootElement = Element("measures")
+                                val document = Document(rootElement)
 
-                    }
-                    Serialisation.XML -> {
-                        val rootElement = Element("measures")
-                        val document = Document(rootElement)
+                                // Measure parsing to XML
+                                for (measure in measureList) {
+                                    val measureElement = Element("measure")
+                                    measureElement.setAttribute("id", measure.id.toString())
+                                    measureElement.setAttribute("status", measure.status.toString())
 
-                        val measures = _measures.value!!
+                                    measureElement.addContent(Element("type").addContent(measure.type.toString()))
+                                    measureElement.addContent(Element("value").addContent(measure.value.toString()))
+                                    measureElement.addContent(Element("date").addContent(toSimpleString(measure.date.time)))
 
-                        // Measure parsing to XML
-                        for (measure in measures) {
-                            val measureElement = Element("measure")
-                            measureElement.setAttribute("id", measure.id.toString())
-                            measureElement.setAttribute("status", measure.status.toString())
+                                    rootElement.addContent(measureElement)
+                                }
 
-                            measureElement.addContent(Element("type").addContent(measure.type.toString()))
-                            measureElement.addContent(Element("value").addContent(measure.value.toString()))
-                            // TODO date format
-                            measureElement.addContent(Element("date").addContent(measure.date.toString()))
+                                val dtdURL = "https://mobile.iict.ch/measures.dtd"
+                                document.docType = DocType("measures", dtdURL)
 
-                            rootElement.addContent(measureElement)
-                        }
+                                val format = org.jdom2.output.Format.getPrettyFormat()
+                                format.textMode = org.jdom2.output.Format.TextMode.PRESERVE
+                                val xmlOutputter = XMLOutputter(format)
 
-                        val dtdURL = "https://mobile.iict.ch/measures.dtd"
-                        document.docType = DocType("measures", dtdURL)
+                                val xmlString = xmlOutputter.outputString(document)
 
-                        val format = org.jdom2.output.Format.getPrettyFormat()
-                        format.textMode = org.jdom2.output.Format.TextMode.PRESERVE
-                        val xmlOutputter = XMLOutputter(format)
-
-                        val xmlString = xmlOutputter.outputString(document)
-
-                        with(URL(url).openConnection() as HttpURLConnection) {
-                            requestMethod = "POST"
-                            setRequestProperty("Content-Type", "application/xml; charset=utf-8")
-                            setRequestProperty("User-Agent", user_agent)
-
-                            try {
-                                // Enable output (sending data to server)
-                                doOutput = true
-
-                                // Write XML data to the connection's output stream
-                                OutputStreamWriter(outputStream, "UTF-8").use { writer ->
+                                OutputStreamWriter(newOutPutStream, "UTF-8").use { writer ->
                                     writer.write(xmlString) //it
                                     writer.flush()
                                 }
-
-                                // Read the response from the server
-                                BufferedReader(InputStreamReader(inputStream)).use { reader ->
-                                    val response = StringBuilder()
-                                    var line: String?
-
-                                    while (reader.readLine().also { line = it } != null) {
-                                        response.append(line)
+                            }
+                            Serialisation.PROTOBUF -> {
+                                val msrs = measures {
+                                    _measures.value!!.forEach {
+                                        measures.add(measure {
+                                            id = it.id
+                                            status = Status.forNumber(it.status.ordinal)
+                                            type = it.type.toString()
+                                            value = it.value
+                                            date = it.date.timeInMillis
+                                        })
                                     }
-
-                                    // Handle response
-                                    val builder = SAXBuilder()
-                                    builder.setFeature("http://xml.org/sax/features/external-general-entities", false);
-                                    val res = builder.build(StringReader(response.toString()))
-
-                                    // Status update
-                                    for (r in res.rootElement.children) {
-                                        val status = Measure.Status.valueOf(r.getAttribute("status").value)
-                                        measures[r.getAttribute("id").intValue].status = status
-                                    }
-
-                                    _measures.postValue(measures)
                                 }
-                            } catch (e: IOException) {
-                                e.printStackTrace()
-                            } finally {
-                                disconnect()
-                            }
-                        }
-                    }
-                    Serialisation.PROTOBUF -> {
-                        val msrs = measures {
-                            _measures.value!!.forEach {
-                                measures.add(measure {
-                                    id = it.id
-                                    status = Status.forNumber(it.status.ordinal)
-                                    type = it.type.toString()
-                                    value = it.value
-                                    date = it.date.timeInMillis
-                                })
-                            }
-                        }
 
-                        with(URL(url).openConnection() as HttpURLConnection) {
-                            requestMethod = "POST"
-                            setRequestProperty("Content-Type", "application/protobuf")
-                            setRequestProperty("User-Agent", user_agent)
-
-                            try {
-                                // Enable output (sending data to server)
-                                doOutput = true
-
-                                // Write Protobuf data to the connection's output stream
-                                DataOutputStream(outputStream).use {
+                                DataOutputStream(newOutPutStream).use {
                                     it.write(msrs.toByteArray())
                                     it.flush()
                                 }
-
-                                // Read the response from the server
-                                DataInputStream(inputStream).use { reader ->
-                                    val bytes = reader.readBytes()
-
-                                    // Handle response
-                                    val measures = _measures.value!!
-                                    val result = MeasuresAck.parseFrom(bytes)
-                                    
-                                    result.measuresList.forEach{ measureAck ->
-                                        measures[measureAck.id].status = Measure.Status.entries[measureAck.status.ordinal]
-                                    }
-
-                                    _measures.postValue(measures)
-                                }
-                            } catch (e: IOException) {
-                                e.printStackTrace()
-                            } finally {
-                                disconnect()
                             }
                         }
+                        newOutPutStream.close()
+
+                        if (responseCode != 200) {
+                            var newInputStream = inputStream
+
+                            if (compression == Compression.DEFLATE) {
+                                newInputStream = InflaterInputStream(newInputStream, Inflater(true))
+                            }
+
+                            when (serialisation) {
+                                Serialisation.JSON -> {
+                                    BufferedReader(InputStreamReader(newInputStream)).use { reader ->
+                                        val response = StringBuilder()
+                                        var line: String?
+                                        while (reader.readLine().also { line = it } != null) {
+                                            response.append(line)
+                                        }
+                                        // Handle response
+                                        val res = gson.fromJson<List<MeasureACK>>(response.toString(), object : TypeToken<List<MeasureACK>>() {}.type)
+
+                                        for (r in res) {
+                                            measureList[r.id].status = r.status
+                                        }
+                                        _measures.postValue(measureList)
+                                    }
+                                }
+                                Serialisation.XML -> {
+                                    BufferedReader(InputStreamReader(newInputStream)).use { reader ->
+                                        val response = StringBuilder()
+                                        var line: String?
+
+                                        while (reader.readLine().also { line = it } != null) {
+                                            response.append(line)
+                                        }
+
+                                        // Handle response
+                                        val builder = SAXBuilder()
+                                        builder.setFeature("http://xml.org/sax/features/external-general-entities", false);
+                                        val res = builder.build(StringReader(response.toString()))
+
+                                        // Status update
+                                        for (r in res.rootElement.children) {
+                                            val status = Measure.Status.valueOf(r.getAttribute("status").value)
+                                            measureList[r.getAttribute("id").intValue].status = status
+                                        }
+
+                                        _measures.postValue(measureList)
+                                    }
+                                }
+                                Serialisation.PROTOBUF -> {
+                                    DataInputStream(newInputStream).use { reader ->
+                                        val bytes = reader.readBytes()
+
+                                        // Handle response
+                                        val measures = _measures.value!!
+                                        val result = MeasuresAck.parseFrom(bytes)
+
+                                        result.measuresList.forEach{ measureAck ->
+                                            measures[measureAck.id].status = Measure.Status.entries[measureAck.status.ordinal]
+                                        }
+
+                                        _measures.postValue(measures)
+                                    }
+                                }
+                            }
+                        } else {
+                            // TODO print message
+                        }
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    } finally {
+                        disconnect()
                     }
                 }
             }
