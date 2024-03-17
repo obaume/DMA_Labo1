e29d987af9380d7a54fd6efc900c9408469860ca
Oscar Baume <oscar.baume@gmail.com>
2024-03-17 15:33:43 +0100

    enfin réussi les requetes post :)

--- code/app/src/main/java/ch/heigvd/iict/dma/labo1/repositories/GraphQLRepository.kt
+++ code/app/src/main/java/ch/heigvd/iict/dma/labo1/repositories/GraphQLRepository.kt
@@ -37,9 +37,7 @@ class GraphQLRepository(private val scope : CoroutineScope, private val httpsUrl
             val elapsed = measureTimeMillis {
                 // TODO make the request to server
                 // fill _authors LiveData with list of all authors
-                val endpoint = "http://mobile.iict.ch/graphql"
-                val query = "{findAllAuthors{id,name}}"
-
+                val endpoint = "https://mobile.iict.ch/graphql"
                 try {
                     val url = URL(endpoint)
                     val conn = url.openConnection() as HttpURLConnection
@@ -47,8 +45,11 @@ class GraphQLRepository(private val scope : CoroutineScope, private val httpsUrl
                     conn.setRequestProperty("Content-Type", "application/json; utf-8")
                     conn.doOutput = true
 
-                    val output = OutputStreamWriter(conn.outputStream)
-                    output.write("{\"query\":\"$query\"}")
+                    val query = "{\"query\":\"{ findAllAuthors {id name}}\"}"
+
+                    val stream = conn.outputStream
+                    val output = OutputStreamWriter(stream)
+                    output.write(query)
                     output.flush()
 
                     if(conn.responseCode == HttpURLConnection.HTTP_OK){
@@ -65,16 +66,19 @@ class GraphQLRepository(private val scope : CoroutineScope, private val httpsUrl
                         // parse json from response
                         val objects = JSONObject(resp.toString())
                                         .getJSONObject("data")
-                                        .getJSONObject("findAllAuthors")
+                                        .getJSONArray("findAllAuthors")
                         val authors = mutableListOf<Author>()
                         for (i in 0 until objects.length()) {
-                            val obj = objects.getJSONObject(i.toString())
+                            val obj = objects.getJSONObject(i)
                             authors.add(Author(obj.getInt("id"), obj.getString("name"), emptyList()))
                         }
 
                         // update _authors
                         _authors.postValue(authors)
+                    } else {
+                        println("Error while connecting")
                     }
+                    conn.disconnect()
                 } catch(e:Exception){
                     println("Exception:${e.message}")
                     _authors.postValue(testAuthors)
@@ -90,8 +94,60 @@ class GraphQLRepository(private val scope : CoroutineScope, private val httpsUrl
                 // TODO make the request to server
                 // fill _books LiveData with list of book of the author
 
-                //placeholder
-                _books.postValue(testBooks)
+
+                val endpoint = "https://mobile.iict.ch/graphql"
+                try {
+                    val url = URL(endpoint)
+                    val conn = url.openConnection() as HttpURLConnection
+                    conn.requestMethod = "POST"
+                    conn.setRequestProperty("Content-Type", "application/json; utf-8")
+                    conn.doOutput = true
+
+                    val query = "{\"query\":\"{ findAuthorById(id:${author.id}) {id name books {id title publicationDate}}}\"}"
+
+                    val stream = conn.outputStream
+                    val output = OutputStreamWriter(stream)
+                    output.write(query)
+                    output.flush()
+
+                    if(conn.responseCode == HttpURLConnection.HTTP_OK){
+                        // get response
+                        val input = BufferedReader(InputStreamReader(conn.inputStream))
+                        var line : String?
+                        val resp = StringBuffer()
+
+                        while(input.readLine().also {line = it} != null) {
+                            resp.append(line)
+                        }
+                        input.close()
+
+                        // parse json from response
+                        val objects = JSONObject(resp.toString())
+                            .getJSONObject("data")
+                            .getJSONObject("findAuthorById")
+                            .getJSONArray("books")
+                        val books = mutableListOf<Book>()
+                        for (i in 0 until objects.length()) {
+                            val obj = objects.getJSONObject(i)
+                            books.add(Book(obj.getInt("id"),
+                                            obj.getString("title"),
+                                            obj.getString("publicationDate"),
+                                            listOf(author)))
+                        }
+
+                        // update _authors
+                        _books.postValue(books)
+                    } else {
+                        println("Error while connecting")
+                    }
+                    conn.disconnect()
+                } catch(e:Exception){
+                    println("Exception:${e.message}")
+                    //placeholder
+                    _books.postValue(testBooks)
+                }
+
+
             }
             _requestDuration.postValue(elapsed)
         }
